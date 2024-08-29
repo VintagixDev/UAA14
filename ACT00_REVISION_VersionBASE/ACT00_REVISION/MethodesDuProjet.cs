376bda248c9bb9af1d1fe855fa664be3d8fcd026
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-08-29 13:37:11 +0200

    Revision

---
+++ ACT00_REVISION_VersionBASE/ACT00_REVISION/MethodesDuProjet.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACT00_REVISION
+{ // écrire les signatures des procédures et fonctions...
+    public struct MethodesDuProjet
+    {
+        // Procédure qui permet de préparer un string 'infos' montrant l'état du triangle selon les situations 'methode' et un booléen 'ok'
+        public void Affiche(bool ok, string methode, out string infos) //nommée Affiche
+        {
+            string verbe;
+            infos = "";
+            // mémorisation du verbe 'est' ou 'n'est pas' pour former la bonne phrase en fonction d'un booléen passé en paramètre
+            if (ok)
+            {
+                verbe = "est ";
+            }
+            else
+            {
+                verbe = "n'est pas ";
+            }
+            // selon la méthode, on prépare la bonne phrase.
+            switch (methode)
+            {
+                case "triangle":
+                    infos = "le polygone " + verbe + "un triangle.";
+                    break;
+                case "equilateral":
+                    infos = "le polygone " + verbe + "un triangle equilateral.";
+                    break;
+                case "isocele":
+                    infos = "le polygone " + verbe + "un triangle isocele.";
+                    break;
+                case "rectangle":
+                    infos = "le polygone " + verbe + "un triangle rectangle.";
+                    break;
+                default:
+                    break;
+            }
+        }
+        // fonction qui calcule la longueur de l'hypothénuse à partir de 2 cotés de type double
+        public double Hypo(double x, double y)// ... nommée Hypo
+        {
+            double z = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            return z;
+        }
+        // fonction qui permet de déterminer si oui ou non un triangle est rectangle en se basant sur ses 3 côtés
+        public bool TriangleRectangle(double a, double b, double c)// nommée TriangleRectangle
+        {
+            bool ok = false;
+            double hypothenuse = Hypo(b, c);
+            if (a == hypothenuse)
+            {
+                ok = true;
+            }
+            return ok;
+            //.... complétez avec ce qui manque logiquement
+        }
+        // procédure qui permet de classer les cotés : on choisi de mettre dans 'a' le plus grand des cotés
+        // les 2 côtés sont modifiés par la procédure
+        public void OrdonneCotes(ref double a, ref double b, ref double c) // nommée ... OrdonneCotes
+        {
+            double temp;
+
+            if ((b >= a) && (b >= c))
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            else if ((c >= a) && (c >= b))
+            {
+                temp = a;
+                a = c;
+                c = temp;
+            }
+        }
+        // fonction booléenne qui détermine si on a un triangle ou pas en se basant sur les longueurs des côtés
+        public bool Triangle(double a, double b, double c)// nommée ...Triangle
+        {
+            bool ok = false;
+
+            OrdonneCotes(ref a, ref b, ref c);
+
+            if (a <= (b + c))
+            {
+                ok = true;
+            }
+            return ok;
+            // ... Compléter ce qui manque...
+        }
+        // procédure qui détermine si un triangle est isocèle ou non sur base des longueurs de côtés
+        public void Isocele(double a, double b, double c, out bool ok)// nommée Isocele
+        {
+            ok = false;
+            if ((a == b) ^ (a == c) ^ (b == c))   // '^' est le 'ou' exclusif (xor)
+            {
+                ok = true;
+            }
+        }
+        // fonction booléenne qui détermine si un triangle est équilatéral sur base de ses côtés
+        public bool Equi(double a, double b, double c)// nommée Equi
+        {
+            bool ok = false;
+            if ((a == b) && (a == c))
+            {
+                ok = true;
+            }
+            return ok;
+        }
+
+        public void LireDouble(string question, out double a)
+        {
+            string aUser;
+            do
+            {
+                Console.WriteLine(question);
+                aUser = Console.ReadLine();
+            } while (!double.TryParse(aUser, out a));
+
+        }
+    }
+}
---
+++ ACT00_REVISION_VersionBASE/ACT00_REVISION/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace ACT00_REVISION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // déclaration des variables.... COMPLETER AVEC CE QUI MANQUE
+            MethodesDuProjet methods = new MethodesDuProjet();
+            string rep;
+            
+            double c1 = 0;
+            double c2 = 0;
+            double c3 = 0;
+            bool ok = false;
+            string infos;
+            // instanciation de la structure
+            // ...... COMPLETER
+
+            Console.WriteLine("Testez les polygones !");
+            //On recommence tant que désiré
+            do
+            {
+                //lecture des 3 côtés
+                c1 = lireDouble(1);
+                c2 = lireDouble(2);
+                c3 = lireDouble(3);
+
+
+                // ordonner les côtés => APPEL ORDONNECOTES
+                methods.OrdonneCotes(ref c1, ref c2, ref c3);
+                // ...
+                // série de test (voir consignes)
+                ok = methods.Triangle(c1, c2, c3);
+                if (ok)
+                {
+                    // préparation et affichage du résultat du test 'triangle' avec la procédure 'Affiche'
+                    methods.Affiche(ok, "triangle", out infos);
+                    Console.WriteLine(infos);
+
+                    ok = methods.Equi(c1, c2, c3);
+                    if (ok)
+                    {
+                        methods.Affiche(ok, "equilateral", out infos);
+                        Console.WriteLine(infos);
+
+                    }
+                    else
+                    {
+                        methods.Affiche(ok, "equilateral", out infos);
+                        Console.WriteLine(infos);
+                        ok = methods.TriangleRectangle(c1, c2, c3);
+                        if (ok)
+                        {
+                            methods.Affiche(ok, "rectangle", out infos);
+                            Console.WriteLine(infos);
+                        }
+                        else
+                        {
+                            // préparation et affichage du résultat négatif du test 'rectangle' avec la procédure 'Affiche'
+                            methods.Affiche(ok, "triangle", out infos);
+                            Console.WriteLine(infos);
+                            // vérification du cas isocèle et affichage dans le cas positif
+
+                            methods.Isocele(c1, c2, c3, out ok);
+                            if (ok)
+                            {
+                                methods.Affiche(ok, "isocele", out infos);
+                                Console.WriteLine(infos);
+                            }
+                            else
+                            {
+                                methods.Affiche(ok, "isocele", out infos);
+                                Console.WriteLine(infos);
+                            }
+                        }
+                        
+                    }
+                }
+                else // si ce n'est pas un triangle
+                {
+                    // préparation et affichage du résultat négataif du test 'triangle' avec la procédure 'Affiche'
+                    methods.Affiche(false, "triangle", out infos);
+                    Console.WriteLine(infos);
+                }
+                // reprise ?
+                Console.WriteLine("Voulez-vous tester un autre polygône ? (Tapez espace)");
+                rep = Console.ReadLine();
+            } while (rep == " ");
+        }
+        //Récupération d'une donnée fournie par l'utilisateur en 'double' : on suppose qu'il ne se trompe pas !
+        static double lireDouble(int numeroCote)
+        {
+            double cote;
+            Console.Write("Tapez la valeur du côté " + numeroCote + " : ");
+            cote = double.Parse(Console.ReadLine());
+            return cote;
+        }
+    }
+}
