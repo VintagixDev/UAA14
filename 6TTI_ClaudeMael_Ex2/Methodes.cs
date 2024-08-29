1d43a9e1c4fba34152d8e03c085993fd17f085b0
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-08-29 13:59:45 +0200

    Exercice 2

---
+++ 6TTI_ClaudeMael_Ex2/Methodes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TTI_ClaudeMael_Ex2
+{
+    public struct Methodes
+    {
+        public void LireDouble(int i, out double x)
+        {
+            string nUser;
+            do
+            {
+                Console.WriteLine("nombre n°"+i);
+                nUser = Console.ReadLine();
+            } while (!double.TryParse(nUser, out x));
+        }
+
+        public void CustomConsole(ConsoleColor bgColor, ConsoleColor textColor)
+        {
+            Console.BackgroundColor = bgColor;
+            Console.ForegroundColor = textColor;
+        
+        }
+    }
+}
---
+++ 6TTI_ClaudeMael_Ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _6TTI_ClaudeMael_Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Methodes methods = new Methodes();
+            
+            methods.CustomConsole(ConsoleColor.Black, ConsoleColor.Cyan);
+
+
+            double[] table = new double[10];
+            double x;
+
+            Console.WriteLine("Entrez 10 nombres afin d'en ressortir la moyenne:");
+            for(int i = 0; i < table.Length; i++) 
+            {
+                methods.LireDouble(i+1, out x);
+                table[i] = x;
+
+            }
+            double addition = 0;
+            for(int i = 0;i < table.Length; i++)
+            {
+                addition += table[i]; 
+            }
+            double final = addition/table.Length;
+            Console.WriteLine("\nMoyenne de tous ces nombres: " + final);
+
+        }
+    }
+}
