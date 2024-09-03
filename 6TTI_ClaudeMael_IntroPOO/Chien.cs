69d323154c0defde8ae72c987361f6e811080020
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-09-03 11:46:17 +0200

    Intro POO

---
+++ 6TTI_ClaudeMael_IntroPOO/Chien.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TTI_ClaudeMael_IntroPOO
+{
+    internal class Chien
+    {
+
+        private int _age;
+        private string _name;
+        private string _race;
+        private double _taille;
+        private double _poids;
+        private string _etat;
+        private bool _puce;
+        public Chien(int age, string name, string race, double taille, double poids, string etat, bool puce)
+        {
+            _age = age;
+            _name = name;
+            _race = race;
+            _taille = taille;
+            _poids = poids;
+            _etat = etat;
+            _puce = puce;
+        }
+
+
+        public string getName()
+        {
+            return _name;
+        }
+
+        public void setName(string name)
+        {
+            _name = name;
+        }
+
+
+        public int getAge()
+        {
+            return _age;
+        }
+
+        public string Manger()
+        {
+            return "Le chien a mangé";
+        }
+
+        public string Boire()
+        {
+            return "Le chien a bu";
+        }
+
+        public void Vieillir()
+        {
+            _age++;
+        }
+
+        public string SeBlesser() 
+        {
+            return "Le chien s'est blessé :(";
+        }
+
+        public string FaireSesBesoins()
+        {
+            return "Le chien a fait ses besoins";
+        }
+
+        public string Mourir()
+        {
+            return "Le chien est mort rip";
+        }
+
+
+
+    }
+}
---
+++ 6TTI_ClaudeMael_IntroPOO/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace _6TTI_ClaudeMael_IntroPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Chien chien = new Chien(3, "Yuki", "Samoyède", 5, 2, "caca", false);
+                Console.WriteLine("1: Manger\n" +
+                    "2: Boire\n" +
+                    "3: Vieillir\n" +
+                    "4: Se blesser\n" +
+                    "5: FaireSesBesoin\n" +
+                    "6: Mourir");
+            do
+            {
+
+
+
+
+
+
+                string nUser;
+                int input;
+                do
+                {
+
+                    nUser = Console.ReadLine();
+                } while (!int.TryParse(nUser, out input));
+
+
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine(chien.Manger());
+                        break;
+                    case 2:
+                        Console.WriteLine(chien.Boire());
+                        break;
+                    case 3:
+                        Console.WriteLine("Le chien avait " + chien.getAge() + " ans");
+                        chien.Vieillir();
+                        Console.WriteLine("Il a maintenant " + chien.getAge() + " ans");
+
+
+                        break;
+                    case 4:
+                        Console.WriteLine(chien.SeBlesser());
+                        break;
+                    case 5:
+                        Console.WriteLine(chien.FaireSesBesoins());
+                        break;
+                    case 6:
+
+                        Console.WriteLine(chien.Mourir());
+                        break;
+                    default: break;
+                }
+            } while (true);
+
+        }
+
+    }
+}
