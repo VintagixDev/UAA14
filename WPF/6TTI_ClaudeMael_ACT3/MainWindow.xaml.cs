ea2ff08226f545865b0303f67600617bf4a8e977
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-09-19 16:08:51 +0200

    act 3

---
+++ WPF/6TTI_ClaudeMael_ACT3/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace _6TTI_ClaudeMael_ACT3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Methodes methods = new Methodes();
+        PageResultat result = new PageResultat();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            BtnV.Visibility = Visibility.Hidden;
+            BtnCalculer.MouseEnter += new MouseEventHandler(SurvolBoutonE);
+            BtnCalculer.MouseLeave += new MouseEventHandler(SurvolBoutonL);
+
+            BtnCalculer.Click += new RoutedEventHandler(BtnCalculer_Click);
+
+
+
+
+            txtA.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
+            txtB.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
+            txtC.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput);
+        }
+
+
+        private void BtnCalculer_Click(object sender, RoutedEventArgs e)
+        {
+            double a;
+            double b;
+            double c;
+            string type;
+            double.TryParse(txtA.Text, out a);
+            double.TryParse(txtB.Text, out b);
+            double.TryParse(txtC.Text, out c);
+            methods.ResoudreTrinome(a, b, c, out type);
+            result.resultText.Text = type;
+            result.Show();
+
+        }
+        private void VerifTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if(e.Text != "," && !EstEntier(e.Text)){
+                e.Handled = true;
+            }
+            if (((TextBox)sender).Text.IndexOf(e.Text) > -1){
+                e.Handled = true;
+            }
+        }
+
+        private void SurvolBoutonE(object sender, MouseEventArgs e)
+        {
+
+            BtnV.Visibility = Visibility.Visible;
+            BtnV.Background = Brushes.Red;
+        }
+        private void SurvolBoutonL(object sender, MouseEventArgs e)
+        {
+
+            BtnV.Visibility = Visibility.Hidden;
+        }
+
+
+        public bool EstEntier(string texteUser)
+        {
+
+            return int.TryParse(texteUser, out int result);
+            
+        }
+    }
+}
---
+++ WPF/6TTI_ClaudeMael_ACT3/Methodes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _6TTI_ClaudeMael_ACT3
+{
+
+    public struct Methodes
+    {
+        
+
+        public void ResoudreTrinome(double a, double b, double c, out string type)
+        {
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+            if (delta < 0)
+            {
+                type = "Il n'y a pas de résultat";
+            }
+            else if (delta == 0)
+            {
+
+                double x1 = -b / (2 * a);
+                type = "Le résultat est " + x1;
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                type = "Les résultats sont " + x1 + " et " + x2;
+            }
+
+            
+            
+        }
+    }
+}
