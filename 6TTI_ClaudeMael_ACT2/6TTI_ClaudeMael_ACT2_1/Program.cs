a0112e7a5645dc53c38bb343caae8c127d90ccda
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-09-24 11:47:51 +0200

    ACT 2

---
+++ 6TTI_ClaudeMael_ACT2/6TTI_ClaudeMael_ACT2_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _6TTI_ClaudeMael_ACT2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nUser;
+            double rayon;
+            do
+            {
+                Console.WriteLine("Veuillez entrer le rayon du cercle.");
+                nUser = Console.ReadLine();
+            } while (!double.TryParse(nUser, out rayon));
+
+            Cercle cercle = new Cercle(rayon);
+
+            Console.WriteLine("Le cercle de rayon " + cercle.Rayon + " a un périmètre de " + cercle.CalculePerimetre() + " et une aire de " + cercle.CalculeAire());
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Avec un cercle de rayon diminué de moitié :");
+            Cercle cercle2 = new Cercle(rayon/2);
+            Console.WriteLine("Le cercle de rayon " + cercle2.Rayon + " a un périmètre de " + cercle2.CalculePerimetre() + " et une aire de " + cercle2.CalculeAire());
+        }
+    }
+}
---
+++ 6TTI_ClaudeMael_ACT2/6TTI_ClaudeMael_ACT2_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Security;
+
+namespace _6TTI_ClaudeMael_ACT2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double reel;
+            double imaginaire;
+            double reelSecondaire;
+            double imaginaireSecondaire;
+            char repeatProg;
+
+            do
+            {
+
+                LireDouble("Que vaut la partie réelle du complexe de départ ?", out reel);
+                LireDouble("Que vaut la partie complexe du complexe de départ ?", out imaginaire);
+
+                Complexe complexe = new Complexe(reel, imaginaire);
+                Console.WriteLine("Le premier complexe : " + complexe.AfficheComplexe() + " a pour module : " + complexe.CalculeModule());
+                Console.WriteLine("\nEncodez un second nombre complexe:");
+
+                LireDouble("Que vaut la partie réelle du second complexe  ?", out reelSecondaire);
+                LireDouble("Que vaut la partie complexe du second complexe ", out imaginaireSecondaire);
+                Complexe complexeSecondaire = new Complexe(reelSecondaire, imaginaireSecondaire);
+                Console.WriteLine("Le second complexe est : " + complexeSecondaire.AfficheComplexe());
+
+                complexe.Ajoute(complexeSecondaire);
+                Console.WriteLine("Le premier complexe devient : " + complexe.AfficheComplexe() + " après l'ajout du second");
+            } while (repeatProg == 'O');
+
+        }
+
+
+
+        static void LireDouble(string question, out double value)
+        {
+            string nUser;
+            do
+            {
+                Console.WriteLine(question);
+                nUser = Console.ReadLine();
+            } while (!double.TryParse(nUser, out value));
+        }
+    }
+}
