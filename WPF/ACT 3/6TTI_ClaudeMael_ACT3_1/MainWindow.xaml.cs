93bbc421ecc57c003378765b12273e1f8b176565
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-09-26 13:07:16 +0200

    ACT 3

---
+++ WPF/ACT 3/6TTI_ClaudeMael_ACT3_1/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace _6TTI_ClaudeMael_ACT3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        Methodes methods = new Methodes(); // Instanciation de la Class Method
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            BtnV.Visibility = Visibility.Hidden;
+            BtnCalculer.MouseEnter += new MouseEventHandler(SurvolBoutonE); // Event de début de survol du bouton Calculer
+            BtnCalculer.MouseLeave += new MouseEventHandler(SurvolBoutonL); // Event de fin de survol du bouton Calculer
+
+            BtnCalculer.Click += new RoutedEventHandler(BtnCalculer_Click); // Event Cliquer sur le bouton Calculer
+
+
+
+
+            txtA.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput); // Event vérification d'input
+            txtB.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput); // Event vérification d'input
+            txtC.PreviewTextInput += new TextCompositionEventHandler(VerifTextInput); // Event vérification d'input
+        }
+
+
+        /// <summary>
+        /// Calculer et ouvrir une nouvelle fenêtre contenant le résultat du trinôme du second degré
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnCalculer_Click(object sender, RoutedEventArgs e)
+        {
+            double a;
+            double b;
+            double c;
+            string type;
+            double.TryParse(txtA.Text, out a);
+            double.TryParse(txtB.Text, out b);
+            double.TryParse(txtC.Text, out c);
+            methods.ResoudreTrinome(a, b, c, out type);
+            PageResultat result = new PageResultat();  // Instanciation de la page du résultat
+            result.resultText.Text = type; // Modifier le texte sur la page du résultat
+            result.Show(); // Afficher la page du réusltat
+
+        }
+
+        /// <summary>
+        /// Vérification des inputs
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void VerifTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if(e.Text != "," && e.Text != "-" && !EstEntier(e.Text)){
+                e.Handled = true;
+            }
+            if (((TextBox)sender).Text.IndexOf(e.Text) > -1){
+                e.Handled = true;
+            }
+        }
+        /// <summary>
+        /// Survol du Bouton "Calculer)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SurvolBoutonE(object sender, MouseEventArgs e)
+        {
+
+            BtnV.Visibility = Visibility.Visible;
+            BtnV.Background = Brushes.Red;
+        }
+        /// <summary>
+        /// Fin de survol du bouton Calculer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SurvolBoutonL(object sender, MouseEventArgs e)
+        {
+
+            BtnV.Visibility = Visibility.Hidden;
+        }
+
+        /// <summary>
+        /// Vérification de l'input, si c'est un entier
+        /// </summary>
+        /// <param name="texteUser"></param>
+        /// <returns></returns>
+        public bool EstEntier(string texteUser)
+        {
+
+            return double.TryParse(texteUser, out double result);
+            
+        }
+    }
+}
---
+++ WPF/ACT 3/6TTI_ClaudeMael_ACT3_1/Methodes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _6TTI_ClaudeMael_ACT3
+{
+
+    public struct Methodes
+    {
+        
+        /// <summary>
+        /// Résolution du trinôme du second degré
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="type"></param>
+        public void ResoudreTrinome(double a, double b, double c, out string type)
+        {
+            double delta = Math.Pow(b, 2) - 4 * a * c; // Calcul du Delta (b²-4ac)
+            if (delta < 0)
+            {
+                type = "Il n'y a pas de résultat";
+            }
+            else if (delta == 0)
+            {
+
+                double x1 = -b / (2 * a); // Calcule racine
+                type = "Le résultat est " + x1;
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a); // Calcul première racine
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a); // Calcul deuxième racine
+                type = "Les résultats sont " + x1 + " et " + x2;
+            }
+
+            
+            
+        }
+    }
+}
