1f317efcd035c2bf64aa851dbe1e7d419edf96de
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-10-03 16:01:21 +0200

    a

--- WPF/ACT 3/6TTI_ClaudeMael_ACT3_bis/MainWindow.xaml.cs
+++ WPF/ACT 3/6TTI_ClaudeMael_ACT3_bis/MainWindow.xaml.cs
@@ -23,6 +23,42 @@ namespace _6TTI_ClaudeMael_ACT3_bis
         public MainWindow()
         {
             InitializeComponent();
+            txtbox_personnes.PreviewTextInput += new TextCompositionEventHandler(VerifIntInput);
+            CalendarDateRange blackoutDays = new CalendarDateRange(new DateTime(0001,01,01), DateTime.Now.AddDays(-1));
+            date_arrivee.BlackoutDates.Add(blackoutDays);
+            date_arrivee.SelectedDate = DateTime.Now;
+            date_arrivee.CalendarClosed += new RoutedEventHandler(SelectDateChangeEvent);
+            date_sortie.BlackoutDates.Add(blackoutDays);
         }
+
+
+        public void SelectDateChangeEvent(object sender, RoutedEventArgs e)
+        {
+            CalendarDateRange blackoutDays = new CalendarDateRange(new DateTime(0001, 01, 01), date_arrivee.SelectedDate.Value.Date);
+            date_sortie.BlackoutDates.Clear();
+            date_sortie.BlackoutDates.Add(blackoutDays);
+        }
+
+        /// <summary>
+        /// Vérifier si l'input est entre 1 et 6
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void VerifIntInput(object sender, TextCompositionEventArgs e) { 
+            if(!int.TryParse(e.Text, out int value))
+            {
+                e.Handled = true;
+            }
+            if (txtbox_personnes.Text.Length > 0)
+            {
+                e.Handled = true;
+            }
+            if(!(value >= 1 && value <= 6))
+            {
+                e.Handled = true;
+            }
+        }
+
+        
     }
 }
