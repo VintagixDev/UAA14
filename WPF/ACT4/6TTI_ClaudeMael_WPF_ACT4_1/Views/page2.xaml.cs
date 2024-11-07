a48cf83b73a14b7829c1e5e06ae3841afe6c6647
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-11-07 16:09:36 +0100

    a

---
+++ WPF/ACT4/6TTI_ClaudeMael_WPF_ACT4_1/Views/page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _6TTI_ClaudeMael_WPF_ACT4.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour page2.xaml
+    /// </summary>
+    public partial class page2 : Page
+    {
+        public page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
