b08031d579ea0403769da4ca738c4a4f92a33cdc
Claude Maël <230622@CCM-Asty-Moulin.local>
2024-11-28 11:30:26 +0100

    act 5

---
+++ WPF/ACT 5/6TTI_ClaudeMael_ACT5_Ex2/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _6TTI_ClaudeMael_ACT5_Ex2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bool alternateColor = false;
+            CreateObjects(alternateColor);
+
+        }
+
+
+        public void CreateObjects(bool color)
+        {
+            Button[,] buttons = new Button[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                gridMain.RowDefinitions.Add(new RowDefinition());
+                gridMain.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            int index = 1;
+            for (int i = 0; i < buttons.GetLength(0); i++)
+            {
+                for (int j = 0; j < buttons.GetLength(1); j++)
+                {
+
+                    Button btn = new Button();
+
+                    btn.Content = index.ToString();
+
+                    
+                    
+                    
+                    btn.Background = Brushes.White;
+                    if (color)
+                    {
+                        btn.Background = Brushes.Black;
+                    }
+                    btn.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    btn.VerticalAlignment = VerticalAlignment.Stretch;
+                    btn.BorderThickness = new Thickness(0, 0, 0, 0);
+                    btn.FontSize = 18;
+                    btn.FontWeight = FontWeights.Bold;
+                    btn.Foreground = Brushes.Red;
+                    Grid.SetRow(btn, i);
+                    if(i % 2 == 0)
+                    {
+                        Grid.SetColumn(btn, j);
+                    }
+                    else
+                    {
+                        Grid.SetColumn(btn, 9-j);
+                    }
+                    buttons[i, j] = btn;
+                    gridMain.Children.Add(btn);
+                    color = !color;
+                    index++;
+                }
+                
+            }
+        }
+    }
+}
---
+++ WPF/ACT 5/6TTI_ClaudeMael_ACT5_Ex3/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _6TTI_ClaudeMael_ACT5_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bool alternateColor = true;
+            CreateObjects(alternateColor);
+
+        }
+
+
+        public void CreateObjects(bool color)
+        {
+            Button[,] buttons = new Button[8, 8];
+            for (int i = 0; i < 8; i++)
+            {
+                gridMain.RowDefinitions.Add(new RowDefinition());
+                gridMain.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            
+
+            for (int i = 0; i < buttons.GetLength(0); i++)
+            {
+                for (int j = 0; j < buttons.GetLength(1); j++)
+                {
+
+
+                    // Création du Button
+                    Button btn = new Button();
+                    
+                    btn.Background = Brushes.White;
+                    if (color)
+                    {
+                        btn.Background = Brushes.Black;
+                    }
+                    btn.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    btn.VerticalAlignment = VerticalAlignment.Stretch;
+                    btn.BorderThickness = new Thickness(0, 0, 0, 0);               
+                    Grid.SetRow(btn, i);
+                    Grid.SetColumn(btn, j);
+                    buttons[i, j] = btn;
+                    gridMain.Children.Add(btn);
+                   
+                    color = !color;
+
+                    // Vérification de la Row
+                    if (i == 0 || i == 1 || i == buttons.GetLength(0)-1 || i == buttons.GetLength(0) - 2)
+                    {
+
+                        Image image = new Image();
+                        BitmapImage bitmap = new BitmapImage();
+                        bitmap.BeginInit();
+                        
+                        bitmap.UriSource = new Uri("Images/p.png", UriKind.Relative);
+
+                        if(i == 0 || i == buttons.GetLength(0) - 1)
+                        {
+                            switch (j)
+                            {
+                                case 0 or 8:
+                                    bitmap.UriSource = new Uri("Images/k.png", UriKind.Relative);
+                                    break;
+                                case 1 or 7:
+                                    bitmap.UriSource = new Uri("Images/kn.png", UriKind.Relative);
+                                    break;
+                                case 2 or 6:
+                                    bitmap.UriSource = new Uri("Images/b.png", UriKind.Relative);
+                                    break;
+                                case 3:
+                                    bitmap.UriSource = new Uri("Images/k.png", UriKind.Relative);
+                                    break;
+                                case 4:
+                                    bitmap.UriSource = new Uri("Images/q.png", UriKind.Relative);
+                                    break;
+                            }
+                        }
+                        bitmap.EndInit();
+                        image.Source = bitmap;
+                        btn.Content = image;
+                    }
+                }
+                color = !color;
+            }
+        }
+    }
+}
